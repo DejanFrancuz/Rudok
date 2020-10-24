0e0c35dfef3f5869b6ccf330485abb2bf2452691
Ignjat <Ignjat@LAPTOP-5UU9MTNF>
2020-10-24 20:16:51 +0200

    Dodata klasa MyMenuBar

---
+++ RuDok/src/dsw/rudok/app/gui/swing/view/MyMenuBar.java
@@ -0,0 +1,13 @@
+package dsw.rudok.app.gui.swing.view;
+
+import javax.swing.*;
+
+public class MyMenuBar extends JMenuBar{
+
+	public MyMenuBar() {
+		JMenu fileMenu = new JMenu("File");
+		
+		this.add(fileMenu);
+	}
+	
+}
