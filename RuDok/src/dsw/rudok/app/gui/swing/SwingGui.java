ae784af68a087086ab4a00733adbc53b4162b293
Ignjat <Ignjat@LAPTOP-5UU9MTNF.mshome.net>
2020-10-27 18:32:09 +0100

    Dopunjena start() metoda

--- RuDok/src/dsw/rudok/app/gui/swing/SwingGui.java
+++ RuDok/src/dsw/rudok/app/gui/swing/SwingGui.java
@@ -1,14 +1,29 @@
 package dsw.rudok.app.gui.swing;
 
+import dsw.rudok.app.core.Gui;
+import dsw.rudok.app.core.Repository;
 import dsw.rudok.app.gui.swing.view.MainFrame;
 
-public class SwingGui{
+public class SwingGui implements Gui{
 
 	private MainFrame instance;
+	private Repository documentRepository;
 	
+	
+	
+	public SwingGui(Repository repository) {
+		
+		this.documentRepository = repository;
+	}
+
+
+
+	@Override
 	public void start() {
 		instance = MainFrame.getInstance();
+		instance.setDocumentRepository(documentRepository);
 		instance.setVisible(true);
-
+		instance.initialiseWorkspaceTree();
+		
 	}
 }
