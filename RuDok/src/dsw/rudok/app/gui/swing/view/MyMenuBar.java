82bfe8cb62feee5f9428b1e6f33678bd44f51fa3
Ignjat <Ignjat@LAPTOP-5UU9MTNF.mshome.net>
2020-10-27 18:34:27 +0100

    Povezane akcije sa ikonicama

--- RuDok/src/dsw/rudok/app/gui/swing/view/MyMenuBar.java
+++ RuDok/src/dsw/rudok/app/gui/swing/view/MyMenuBar.java
@@ -1,11 +1,19 @@
 package dsw.rudok.app.gui.swing.view;
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.*;
+
 
 public class MyMenuBar extends JMenuBar{
 
 	public MyMenuBar() {
 		JMenu fileMenu = new JMenu("File");
+		
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		fileMenu.add(MainFrame.getInstance().getActionManager().getExitAction());
+		fileMenu.add(MainFrame.getInstance().getActionManager().getNewProjectAction());
+		
 		
 		this.add(fileMenu);
 	}
--- RuDok/src/dsw/rudok/app/gui/swing/view/Toolbar.java
+++ RuDok/src/dsw/rudok/app/gui/swing/view/Toolbar.java
@@ -7,6 +7,10 @@ public class Toolbar extends JToolBar{
 	public Toolbar() {
 		super(HORIZONTAL);
 		setFloatable(false);
+		
+		add(MainFrame.getInstance().getActionManager().getExitAction());
+		add(MainFrame.getInstance().getActionManager().getNewProjectAction());
+		
 	}
 	
 }
