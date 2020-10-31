31784f5603e80919718e73b91d47ab390a2317e2
DejanFrancuz <dfrnacuz7620rn@raf.rs>
2020-10-31 13:43:11 +0100

    Implementiran TreeView i TreeImplementation

---
+++ RuDok/src/dsw/rudok/app/gui/swing/tree/view/RuTreeView.java
@@ -0,0 +1,19 @@
+package dsw.rudok.app.gui.swing.tree.view;
+
+import dsw.rudok.app.gui.swing.tree.controller.RuTreeCellEditor;
+import dsw.rudok.app.gui.swing.tree.controller.RuTreeSelectionListener;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultTreeModel;
+
+public class RuTreeView extends JTree {
+
+    public RuTreeView(DefaultTreeModel defaultTreeModel){
+        setModel(defaultTreeModel);
+        RuTreeCellRender ruTreeCellRender = new RuTreeCellRender();
+        addTreeSelectionListener(new RuTreeSelectionListener());
+        setCellEditor(new RuTreeCellEditor(this,ruTreeCellRender));
+        setCellRenderer(ruTreeCellRender);
+        setEditable(true);
+    }
+}
